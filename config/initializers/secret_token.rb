91cdcb38e14f3b88de9d8ee2c5cd5c9f508627ac
Reiner Lim <reinerlim17@gmail.com>
2012-08-23 14:31:25 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroTest::Application.config.secret_token = '4f8b42c5745037742279f24fd693811b35639b08adfbd46cdcd24bf4dfe02d57c5aad5f9dcabc001d2e343a436951a84088838b09da22c35d30f8c78c307150f'
